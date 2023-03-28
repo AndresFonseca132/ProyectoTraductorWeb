3c161fcd8a6ac8cf91dd27fa9f74dbd743f34500
Jahernandez0465 <juanniviaa@gmail.com>
2023-03-28 11:38:56 -0500

    App route

--- TraductorWeb/app.py
+++ TraductorWeb/app.py
@@ -12,8 +12,13 @@ def hello_world():  # put application's code here
 def signin():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('registro.html')
 
-
+@app.route('/reset_password')
+def rpassword():
+    return render_template('recuperar_contras.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=2502)
